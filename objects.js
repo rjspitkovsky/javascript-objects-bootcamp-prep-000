8989f1c4146c4f7d9b0fd614611d07c52149d360
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-11-14 18:21:56 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {
+  arcticMonkeys : 'arabella'
+  linkinPark: 'numb'
+  disclosure: 'nocturnal'
+}
+
+function updatePlaylist(object,bandname,title) {
+  object.bandname = "title" 
+  return object 
+}
